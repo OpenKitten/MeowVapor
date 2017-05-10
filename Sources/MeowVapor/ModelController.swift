4edd6f82e56b963aedc924aac6d55fe07fd91144
Robbert Brandsma <i@robbertbrandsma.nl>
2017-05-10 15:10:59 +0200

    Separate authorization checking

--- Sources/MeowVapor/ModelController.swift
+++ Sources/MeowVapor/ModelController.swift
@@ -13,13 +13,10 @@ extension BaseModel where Self : StringInitializable {
 
 open class ModelController<M : Model & StringInitializable>: ResourceRepresentable {
     
-    public typealias AuthorizationChecker = (Request) throws -> Void
-    
     public init() {}
     
     open var filterFields: Set<M.Key> = []
     open var sortFields: Set<M.Key> = []
-    open var authorizationChecker: AuthorizationChecker?
     
     open func makeResource() -> Resource<M> {
         return Resource(
@@ -32,22 +29,16 @@ open class ModelController<M : Model & StringInitializable>: ResourceRepresentab
     }
     
     open func show(request: Request, instance: M) throws -> ResponseRepresentable {
-        try authorizationChecker?(request)
-        
         return instance.serialize()
     }
     
     open func destroy(request: Request, instance: M) throws -> ResponseRepresentable {
-        try authorizationChecker?(request)
-        
         try instance.delete()
         
         return Response(status: .noContent)
     }
     
     open func modify(request: Request, instance: M) throws -> ResponseRepresentable {
-        try authorizationChecker?(request)
-        
         guard let document = request.document else {
             throw Abort.badRequest
         }
@@ -58,8 +49,6 @@ open class ModelController<M : Model & StringInitializable>: ResourceRepresentab
     }
     
     open func index(request: Request) throws -> ResponseRepresentable {
-        try authorizationChecker?(request)
-        
         let (result, usedPagination) = try M.paginatedFind(for: request,
                                          allowFiltering: filterFields,
                                          allowSorting: sortFields)
@@ -82,8 +71,6 @@ open class ModelController<M : Model & StringInitializable>: ResourceRepresentab
     }
     
     open func store(request: Request) throws -> ResponseRepresentable {
-        try authorizationChecker?(request)
-        
         guard let document = request.document else {
             throw Abort.badRequest
         }
@@ -91,6 +78,79 @@ open class ModelController<M : Model & StringInitializable>: ResourceRepresentab
         _ = try M(newFrom: document)
         
         return Response(status: .noContent)
+    }
+    
+}
+
+open class ClosureBasedAccessControlModelController<M : Model & StringInitializable> : ModelController<M> {
+    
+    public typealias MultipleAccessChecker = (Request) throws -> Void
+    public typealias SingleAccessChecker = (Request, M) throws -> Void
+    
+    /// A closure that runs before every operation.
+    /// Throw from the closure to prevent the request from executing.
+    open var genericChecker: MultipleAccessChecker?
+    
+    /// A closure that runs before every operation on a single instance.
+    /// Throw from the closure to prevent the request from executing.
+    open var instanceAccessChecker: SingleAccessChecker?
+    
+    /// A closure that runs before every index operation.
+    /// Throw from the closure to prevent the request from executing.
+    open var indexAccessChecker: MultipleAccessChecker?
+    
+    /// A closure that runs before every store operation.
+    /// Throw from the closure to prevent the request from executing.
+    open var storeAccessChecker: MultipleAccessChecker?
+    
+    /// A closure that runs before every show operation.
+    /// Throw from the closure to prevent the request from executing.
+    open var showAccessChecker: SingleAccessChecker?
+    
+    /// A closure that runs before every modify operation.
+    /// Throw from the closure to prevent the request from executing.
+    open var modifyAccessChecker: SingleAccessChecker?
+    
+    /// A closure that runs before every destroy operation.
+    /// Throw from the closure to prevent the request from executing.
+    open var destroyAccessChecker: SingleAccessChecker?
+    
+    open override func show(request: Request, instance: M) throws -> ResponseRepresentable {
+        try genericChecker?(request)
+        try instanceAccessChecker?(request, instance)
+        try showAccessChecker?(request, instance)
+        
+        return try super.show(request: request, instance: instance)
+    }
+    
+    open override func store(request: Request) throws -> ResponseRepresentable {
+        try genericChecker?(request)
+        try storeAccessChecker?(request)
+        
+        return try super.store(request: request)
+    }
+    
+    open override func index(request: Request) throws -> ResponseRepresentable {
+        try genericChecker?(request)
+        try indexAccessChecker?(request)
+        
+        return try super.index(request: request)
+    }
+    
+    open override func modify(request: Request, instance: M) throws -> ResponseRepresentable {
+        try genericChecker?(request)
+        try instanceAccessChecker?(request, instance)
+        try modifyAccessChecker?(request, instance)
+        
+        return try super.modify(request: request, instance: instance)
+    }
+    
+    open override func destroy(request: Request, instance: M) throws -> ResponseRepresentable {
+        try genericChecker?(request)
+        try instanceAccessChecker?(request, instance)
+        try destroyAccessChecker?(request, instance)
+        
+        return try super.destroy(request: request, instance: instance)
     }
     
 }

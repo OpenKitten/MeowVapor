6a989526fe9ae08adeb4799869f205fd26bb4405
Joannis Orlandos <joannis@orlandos.nl>
2017-05-11 14:23:11 +0200

    More docs

--- Sources/MeowVapor/ResponseRepresentable.swift
+++ Sources/MeowVapor/ResponseRepresentable.swift
@@ -4,6 +4,7 @@ import BSON
 import HTTP
 
 extension Document : ResponseRepresentable {
+    /// Returns a BSON Document as a Response
     public func makeResponse() throws -> Response {
         return Response(status: .ok, headers: [
             "Content-Type": "application/json; charset=utf-8"
@@ -12,6 +13,7 @@ extension Document : ResponseRepresentable {
 }
 
 extension JSONObject : ResponseRepresentable {
+    /// Returns a Cheetah JSONObject as a Response
     public func makeResponse() throws -> Response {
         return Response(status: .ok, headers: [
             "Content-Type": "application/json; charset=utf-8"
@@ -20,6 +22,7 @@ extension JSONObject : ResponseRepresentable {
 }
 
 extension JSONArray : ResponseRepresentable {
+    /// Returns a Cheetah JSONArray as a Response
     public func makeResponse() throws -> Response {
         return Response(status: .ok, headers: [
             "Content-Type": "application/json; charset=utf-8"

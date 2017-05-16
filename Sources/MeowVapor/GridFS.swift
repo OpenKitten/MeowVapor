aa49c16ec6dd05317a2938a5958dbc0c258b8264
Robbert Brandsma <i@robbertbrandsma.nl>
2017-05-16 14:47:44 +0200

    GridFS improvements

--- Sources/MeowVapor/GridFS.swift
+++ Sources/MeowVapor/GridFS.swift
@@ -4,31 +4,75 @@ import MongoKitten
 import HTTP
 import Vapor
 
-extension File : ResponseRepresentable {
+extension GridFS.File : ResponseRepresentable {
     /// Returns a GridFS file as a response
     public func makeResponse() throws -> Response {
-        guard let file = try GridFS.default.findOne(byID: id) else {
-            throw Abort.notFound
-        }
-        
         var headers: [HeaderKey: String] = [:]
         
-        if let type = file.contentType {
+        if let type = self.contentType {
             headers["Content-Type"] = type
         }
         
         return Response(status: .ok, headers: headers) { stream in
-            for chunk in file {
+            for chunk in self {
                 try stream.write(chunk.data)
             }
             
             try stream.close()
         }
     }
+    
+    public static func from(_ request: Request, allowing contentTypes: [String]? = nil) throws -> GridFS.File {
+        let file = try request.getFile(allowing: contentTypes)
+        
+        let id = try Meow.fs.store(data: file.data,
+                                   named: file.name,
+                                   withType: file.contentType)
+        
+        return try GridFS.File.restore(id)
+    }
 }
 
-extension Optional where Wrapped : ResponseRepresentable {
-    /// Returns an optional if it contains something
+extension Request {
+    func getFile(allowing contentTypes: [String]? = nil) throws -> (contentType: String, name: String?, data: [UInt8]) {
+        if let form = self.formData {
+            guard let part = form["file"]?.part else {
+                throw Abort(.badRequest,
+                                 reason: "File not found in multipart")
+            }
+            
+            guard let type = part.headers["Content-Type"] else {
+                throw Abort(.badRequest,
+                            reason: "File type not present in multipart")
+            }
+            
+            if let contentTypes = contentTypes {
+                guard contentTypes.contains(type) else {
+                    throw Abort(.badRequest,
+                                reason: "Content type \(type) not allowed")
+                }
+            }
+            
+            // TODO: break here? check how to get filename
+            return (contentType: type, name: "", data: part.body)
+        }
+        
+        if let type = self.contentType, (contentTypes?.contains(type) ?? true) {
+            guard let bytes = self.body.bytes else {
+                throw Abort(.badRequest,
+                            reason: "Request does not contain body")
+            }
+            
+            return (contentType: type, name: nil, data: bytes)
+        }
+        
+        throw Abort(.badRequest,
+                    reason: "Could not get file from request")
+    }
+
+}
+
+public extension Optional where Wrapped == GridFS.File {
     public func makeResponse() throws -> Response {
         guard let wrapped = self else {
             throw Abort.notFound

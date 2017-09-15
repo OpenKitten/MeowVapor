e23f8cdced79db05a36049447a99bf098c575fb9
Joannis Orlandos <joannis@orlandos.nl>
2017-09-15 16:59:59 +0200

    bson lock

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/OpenKitten/Meow.git", .revision("master/1.0")),
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", .revision("master/4.1")),
+        .package(url: "https://github.com/OpenKitten/BSON.git", .revision("master/5.2")),
         .package(url: "https://github.com/vapor/vapor.git", from: "2.2.0")
     ],
     targets: [

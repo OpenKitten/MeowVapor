daa194c6e56f0d56c26c3b1c96e5b7919a14a4fe
Robbert Brandsma <i@robbertbrandsma.nl>
2018-05-30 23:54:59 +0200

    Parameterizable implementation

---
+++ Sources/MeowVapor/MeowVaporError.swift
@@ -0,0 +1,3 @@
+enum MeowVaporError: Error {
+    case modelInParameterNotFound
+}
---
+++ Sources/MeowVapor/Parameterizable.swift
@@ -0,0 +1,46 @@
+//
+//  Parameterizable.swift
+//  MeowVapor
+//
+//  Created by Robbert Brandsma on 20/06/2017.
+//
+
+import Meow
+import Vapor
+
+public extension Model where Self: Parameter, Self.Identifier == ObjectId {
+    public static var uniqueSlug: String {
+        return String(describing: Self.self)
+    }
+    
+    static func resolveParameter(_ parameter: String, on container: Container) throws -> EventLoopFuture<Self> {
+        let id = try ObjectId(parameter)
+        let context = try container.make(Meow.Context.self)
+        
+        return context.findOne(Self.self, query: "_id" == id).thenThrowing { instance in
+            guard let instance = instance else {
+                throw MeowVaporError.modelInParameterNotFound
+            }
+            
+            return instance
+        }
+    }
+}
+
+public extension Model where Self: Parameter, Self.Identifier == String {
+    public static var uniqueSlug: String {
+        return String(describing: Self.self)
+    }
+    
+    static func resolveParameter(_ parameter: String, on container: Container) throws -> EventLoopFuture<Self> {
+        let context = try container.make(Meow.Context.self)
+        
+        return context.findOne(Self.self, query: "_id" == parameter).thenThrowing { instance in
+            guard let instance = instance else {
+                throw MeowVaporError.modelInParameterNotFound
+            }
+            
+            return instance
+        }
+    }
+}

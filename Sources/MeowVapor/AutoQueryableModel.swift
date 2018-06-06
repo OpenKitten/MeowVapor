f36424f082a1428ebc56442cf643c185d8fb4daf
Robbert Brandsma <i@robbertbrandsma.nl>
2018-06-06 17:08:17 +0200

    AutoQueryable based on Vapor Core

---
+++ Sources/MeowVapor/AutoQueryableModel.swift
@@ -0,0 +1,62 @@
+import Meow
+import Core
+import BSON
+
+public enum AutoQueryableModelError: Error {
+    case propertyNotFound
+    case tooFewEnumCases
+}
+
+public extension QueryableModel {
+    static func makeQueryPath<T>(for key: KeyPath<Self, T>) throws -> String {
+        guard let property = try Self.decodeProperty(forKey: key) else {
+            throw AutoQueryableModelError.propertyNotFound
+        }
+        
+        return property.path.joined(separator: ".")
+    }
+}
+
+extension ObjectId: ReflectionDecodable {
+    fileprivate static func leftDecoded() throws -> ObjectId {
+        return try ObjectId("000000000000000000000000")
+    }
+    
+    fileprivate static func rightDecoded() throws -> ObjectId {
+        return try ObjectId("ffffffffffffffffffffffff")
+    }
+    
+    public static func reflectDecoded() throws -> (ObjectId, ObjectId) {
+        return try (leftDecoded(), rightDecoded())
+    }
+    
+    public static func reflectDecodedIsLeft(_ item: ObjectId) throws -> Bool {
+        return try leftDecoded() == item
+    }
+}
+
+extension Reference: AnyReflectionDecodable where M.Identifier == ObjectId {}
+extension Reference: ReflectionDecodable where M.Identifier == ObjectId {
+    private static func leftDecoded() throws -> Reference<M> {
+        return try Reference(uncheckedTo: ObjectId.leftDecoded())
+    }
+    
+    private static func rightDecoded() throws -> Reference<M> {
+        return try Reference(uncheckedTo: ObjectId.rightDecoded())
+    }
+    
+    public static func reflectDecoded() throws -> (Reference<M>, Reference<M>) {
+        return try (leftDecoded(), rightDecoded())
+    }
+}
+
+extension ReflectionDecodable where Self: CaseIterable {
+    public static func reflectDecoded() throws -> (Self, Self) {
+        let cases = Array(Self.allCases)
+        guard cases.count >= 2 else {
+            throw AutoQueryableModelError.tooFewEnumCases
+        }
+        
+        return (cases[0], cases[1])
+    }
+}

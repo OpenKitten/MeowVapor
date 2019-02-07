ac7595d751d57862dad1250aafc9a036e269d716
Robbert Brandsma <i@robbertbrandsma.nl>
2019-02-07 12:30:44 +0100

    Added a default case to indicate that switching over the MeowVaporError enum isn't really a good idea

--- Sources/MeowVapor/MeowVaporError.swift
+++ Sources/MeowVapor/MeowVaporError.swift
@@ -1,3 +1,6 @@
 public enum MeowVaporError: Error {
     case modelInParameterNotFound
+    
+    @available(*, deprecated, message: "Don't switch over this enum without a default case, because we may add more cases in the futrure, and that will break your code")
+    case dontSwitchOverThisEnumWithoutADefaultCaseBecauseWeMayAddMoreCasesInTheFutureAndThatWillBreakYourCode
 }

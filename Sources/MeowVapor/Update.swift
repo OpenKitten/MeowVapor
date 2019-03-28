74f0bea5c0bd475cf4cdf072143093cdc9322408
Robbert Brandsma <i@robbertbrandsma.nl>
2019-03-28 11:51:02 +0100

    Loosen requirement of `update` on `ContentContainer` from `QueryableModel` to `KeyPathQueryable`

--- Sources/MeowVapor/Update.swift
+++ Sources/MeowVapor/Update.swift
@@ -3,7 +3,7 @@ import Vapor
 import Foundation
 
 extension ContentContainer {
-    public func update<D: QueryableModel>(_ instance: D, maxSize: Int = 65_536, using decoder: JSONDecoder = JSONDecoder(), withAllowedKeyPaths keyPaths: [MeowWritableKeyPath]) throws -> Future<[PartialKeyPath<D>]> {
+    public func update<D: KeyPathQueryable>(_ instance: D, maxSize: Int = 65_536, using decoder: JSONDecoder = JSONDecoder(), withAllowedKeyPaths keyPaths: [MeowWritableKeyPath]) throws -> Future<[PartialKeyPath<D>]> {
         return try decode(DecoderExtractor.self, maxSize: maxSize, using: decoder).map { extracted in
             let decoder = extracted.decoder
             return try decoder.update(instance, withAllowedKeyPaths: keyPaths)
